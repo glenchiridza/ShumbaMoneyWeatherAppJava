3e3e5fe099ee58a588405a04ef3a3908fccd50ac
glenchiridza <glenchiridz@gmail.com>
2021-11-14 02:47:27 +0200

    splash done

--- app/src/main/java/com/glencconnnect/shumbamoneweather/SplashScreenActivity.java
+++ app/src/main/java/com/glencconnnect/shumbamoneweather/SplashScreenActivity.java
@@ -2,13 +2,22 @@ package com.glencconnnect.shumbamoneweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ///handler to simulate delay and switch from splashscreen into MainActivity
+        handler.postDelayed(() ->
+                startActivity(new Intent(SplashScreenActivity.this,MainActivity.class)
+                ),3000);
     }
 }
