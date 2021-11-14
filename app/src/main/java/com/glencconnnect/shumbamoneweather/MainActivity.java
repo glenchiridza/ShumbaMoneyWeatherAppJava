e05f287c057ff2fddde2e05e54016f6027168101
glenchiridza <glenchiridz@gmail.com>
2021-11-14 18:36:32 +0200

    about

--- app/src/main/java/com/glencconnnect/shumbamoneweather/MainActivity.java
+++ app/src/main/java/com/glencconnnect/shumbamoneweather/MainActivity.java
@@ -1,10 +1,14 @@
 package com.glencconnnect.shumbamoneweather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.glencconnnect.shumbamoneweather.adapters.WeatherRecyclerAdapter;
 
@@ -26,5 +30,30 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == R.id.action_about){
+
+        }
+        else if(id == R.id.action_credits){
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void shareIntent(){
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        startActivity(Intent.createChooser(shareIntent,"Share App"));
     }
 }
